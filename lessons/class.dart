254c555b74dfab5a3d7e1c72309810170120114d
Ozcan Bayram <ozzcanbayram@gmail.com>
2024-05-10 17:42:55 +0300

    class.dart (1)

---
+++ lessons/class.dart
@@ -0,0 +1,64 @@
+void main() {
+
+  //null
+  int? money;
+  if(money != null){
+    print(money+10);
+  }
+
+  // bankaya 3 tane musteri gelecek birinin 100 tlsi var birinin hesabi yok bir diğerinin 0 tlsi var
+  // hesabi olmayana gelin hesap acalim, parai olmayanı kov, 100 tlsi olana hosgeldin de
+
+  //yeni bir meyhod olsun hesabı olmayanlar ve parası olmayanlar yok sayılsın
+  List<int?> customersMoney = [100,null,0];
+
+  for (var item in customersMoney) {
+      if(item != null){
+        if(item <= 0){
+          print('Kovuldun.');
+        }
+        else{
+          print('Hoşgeldiniz.');
+        }
+      } 
+      else{
+        print('Gelin size hesap açalım.');
+      }
+
+      bool result = controlMoney(item) == null ? false : true; //parası olmayanlar ve hesabı olmayanlar için null yani false döner.
+      print(result);   
+  } 
+
+  print('--------------');
+
+  //class kullanimi:
+
+  User user1 = User('Ozcan', 2000, 21, 'Ankara'); // User sınıfından user1 nesnesini oluşturduk.
+
+}
+
+int? controlMoney(int? money) {
+  if(money == null || money == 0){
+    return null;
+  }
+}
+
+
+//adı olmak zorunda
+//parası olmak zorunda
+//yaşını vermeyebilir
+//city vermeyebilir 
+class User{
+  late final String name; //Late ile constructor esnasında yani daha sonra değer atanacağını belirtiriz.
+  late final int money;
+  late final int age;
+  late final String city;
+
+  //Constructor
+  User(String name, int money, int age, String city){
+    this.name = name;
+    this.money = money;
+    this.age = age;
+    this.city = city;
+  }
+}
